88d7a8255aa62c7e40b8526bc29377becab0cbb2
jang_yoonsu <shagrat8546@gmail.com>
2024-05-30 23:22:19 +0900

    chore : add github-api-url, github-access-token env config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    GITHUB_API_URL: process.env.GITHUB_API_URL,
+    GITHUB_ACCESS_TOKEN: process.env.GITHUB_ACCESS_TOKEN,
+  },
+};
 
 export default nextConfig;
