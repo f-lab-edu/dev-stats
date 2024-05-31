40aeb5d8a23d2be255c87bda9b404c04e91bca1c
jang_yoonsu <shagrat8546@gmail.com>
2024-05-31 22:30:12 +0900

    rename : change file name to fetchGithub.ts from index.ts and change variable name

--- src/apis/fetchGithub.ts
+++ src/apis/fetchGithub.ts
@@ -1,9 +1,9 @@
-import { request } from "./utils/request";
+import { Request } from "./utils/request";
 
 const GITHUB_API_URL = process.env.NEXT_PUBLIC_GITHUB_API_URL!;
 const GITHUB_ACCESS_TOKEN = process.env.NEXT_PUBLIC_GITHUB_ACCESS_TOKEN!;
 
-export const fetchBase = request.create({
+export const fetchGithub = Request.create({
   baseURL: GITHUB_API_URL,
   headers: {
     Authorization: `token ${GITHUB_ACCESS_TOKEN}`,
