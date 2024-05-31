f168aaaa2b03b874716bc55ca69e7ec06a25d3c1
jang_yoonsu <shagrat8546@gmail.com>
2024-05-31 23:49:54 +0900

    feat : add index.ts in apis

---
+++ src/apis/index.ts
@@ -0,0 +1,6 @@
+export * from "./fetchGithub";
+export * from "./getUserLanguages";
+export * from "./getUserOrganizations";
+export * from "./getUserProfile";
+export * from "./getUserRepos";
+export * from "./utils";
