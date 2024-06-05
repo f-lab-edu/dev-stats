c4deadb6f1905a8492eabc5bb01d9c8bcfe0dac8
jang_yoonsu <shagrat8546@gmail.com>
2024-06-06 01:47:33 +0900

    chore : add github images domain in nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
     GITHUB_API_URL: process.env.GITHUB_API_URL,
     GITHUB_ACCESS_TOKEN: process.env.GITHUB_ACCESS_TOKEN,
   },
+  images: {
+    domains: ["avatars.githubusercontent.com"],
+  },
 };
 
 export default nextConfig;
