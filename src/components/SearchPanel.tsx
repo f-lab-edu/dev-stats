6b61e1f729de71480e897bb69ffd61c76bd8d121
sossost <shagrat8546@gmail.com>
2024-06-07 23:19:14 +0900

    refactor : separate Fallback and UserList component

--- src/components/SearchPanel.tsx
+++ src/components/SearchPanel.tsx
@@ -2,13 +2,12 @@
 
 import { cva } from "class-variance-authority";
 import { ClassValue } from "clsx";
-import Image from "next/image";
-import Link from "next/link";
+scrollBy;
 
 import { useSearchUserQuery } from "@/hooks";
 import { cn } from "@/utils";
 
-import { LoadingSpinner } from "./LoadingSpinner";
+import { SearchedUserList } from "./UserList";
 
 type SearchPanelProps = {
   isVisible?: boolean;
@@ -24,74 +23,16 @@ export const SearchPanel = ({
   className,
   ...props
 }: SearchPanelProps) => {
+  const { searchedUsers, state } = useSearchUserQuery(searchQuery);
+
   if (!isVisible) return null;
 
   return (
     <div className={cn([ContainerVariants({ size }), className])} {...props}>
       <hr className="mx-4 bg-blue-200" />
-      <SearchedUserList searchQuery={searchQuery} />
+      <SearchedUserList userList={searchedUsers} state={state} />
     </div>
   );
-};
-
-type SearchedUserListProps = {
-  searchQuery: string;
-};
-
-const SearchedUserList = ({ searchQuery }: SearchedUserListProps) => {
-  const { searchedUsers, isInitialPending, isNoResult } =
-    useSearchUserQuery(searchQuery);
-
-  if (isInitialPending) {
-    return <Fallback state="PENDING" />;
-  }
-
-  if (isNoResult) {
-    return <Fallback state="NO_RESULT" />;
-  }
-
-  return (
-    <ul className="pt-2 h-full">
-      {searchedUsers.map(user => (
-        <li key={user.login}>
-          <Link
-            href={`/${user.login}`}
-            passHref={true}
-            className="
-              flex items-center py-1 px-4 hover:bg-blue-50 
-              cursor-pointer gap-3
-             "
-          >
-            <Image
-              src={user.avatar_url}
-              alt={user.login}
-              width={24}
-              height={24}
-              placeholder="empty"
-              className="
-                rounded-full outline-1 outline
-                bg-blue-100  outline-blue-100
-              "
-            />
-            {user.login}
-          </Link>
-        </li>
-      ))}
-    </ul>
-  );
-};
-
-type FallbackProps = {
-  state: "PENDING" | "NO_RESULT";
-};
-
-const Fallback = ({ state }: FallbackProps) => {
-  const FALLBACK = {
-    PENDING: <LoadingSpinner />,
-    NO_RESULT: "No results found",
-  };
-
-  return <div className="flex-row-center full-size">{FALLBACK[state]}</div>;
 };
 
 const ContainerVariants = cva(
