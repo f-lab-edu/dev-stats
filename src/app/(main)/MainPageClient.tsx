02920664a9a85cdaab9875291c9e586ba345b2bd
jang_yoonsu <shagrat8546@gmail.com>
2024-06-08 19:27:48 +0900

    refactor : Refactor MainPageClient component to include responsive layout in MainPageClient.tsx

--- src/app/(main)/MainPageClient.tsx
+++ src/app/(main)/MainPageClient.tsx
@@ -4,7 +4,13 @@ import { Logo, Search } from "@/components";
 
 const MainPageClient = () => {
   return (
-    <div className="flex flex-col justify-center items-center w-full h-full gap-10 pb-40 px-5">
+    <div
+      className="
+      flex flex-col items-center w-full h-full gap-10 pb-40 px-5 pt-10
+      md:pt-32
+      lg:justify-center lg:pt-0
+      "
+    >
       <Logo size="lg" />
       <div className="relative flex flex-col items-center w-full">
         <Search size="lg" />
