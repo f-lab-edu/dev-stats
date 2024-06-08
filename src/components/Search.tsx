efb576407b3dfc5f1e804776a0b873c14a392a6e
jang_yoonsu <shagrat8546@gmail.com>
2024-06-08 19:16:10 +0900

    feat : add Search component

---
+++ src/components/Search.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import React from "react";
+
+import { cn } from "@/utils";
+import { useSearchUser } from "@/hooks";
+
+import { SearchBar } from "./SearchBar";
+import { SearchPanel } from "./SearchPanel";
+
+type SearchProps = {
+  size?: "sm" | "lg";
+};
+
+export const Search = ({ size = "sm" }: SearchProps) => {
+  const { searchQuery, handleSearchButtonClick, handleSearchQueryChange } =
+    useSearchUser();
+
+  const containerRadiusClassName =
+    searchQuery.length < 3 ? "" : "rounded-bl-none rounded-br-none";
+
+  const maxWidthClassName = {
+    sm: "max-w-[400px]",
+    lg: "max-w-[500px]",
+  };
+
+  return (
+    <>
+      <SearchBar
+        size={size}
+        value={searchQuery}
+        onChange={handleSearchQueryChange}
+        onSearch={handleSearchButtonClick}
+        containerClassName={cn([
+          containerRadiusClassName,
+          maxWidthClassName[size],
+        ])}
+      />
+      <SearchPanel
+        size={size}
+        isVisible={searchQuery.length > 2}
+        searchQuery={searchQuery}
+        className={maxWidthClassName[size]}
+      />
+    </>
+  );
+};
