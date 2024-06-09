b2683c8783bd87726d28a0a208ff43ae9daedd44
jang_yoonsu <shagrat8546@gmail.com>
2024-06-09 18:40:50 +0900

    refactor : Refactor getUserLanguages function to improve readability and performance

--- src/apis/getUserLanguages.ts
+++ src/apis/getUserLanguages.ts
@@ -47,19 +47,28 @@ const getUserReposWithLanguages = async (username: string) => {
   return result.data.user.repositories.nodes;
 };
 
+const extractLanguages = (repos: RepoWithLanguages[]) => {
+  return repos.reduce((acc, repo) => {
+    repo.languages.edges.forEach(({ node, size }) => {
+      acc[node.name] = (acc[node.name] || 0) + size;
+    });
+    return acc;
+  }, {} as LanguageStats);
+};
+
+const calculatePercent = (data: CalculatePercentProps) => {
+  const total = Object.values(data).reduce((acc, value) => acc + value, 0);
+  return Object.fromEntries(
+    Object.entries(data).map(([key, value]) => [key, (value / total) * 100]),
+  );
+};
+
 export const getUserLanguages = async (username: string) => {
   const repos = await getUserReposWithLanguages(username);
-  const languageStats: LanguageStats = {};
-
-  repos.forEach((repo: RepoWithLanguages) => {
-    repo.languages.edges.forEach(({ node, size }) => {
-      if (languageStats[node.name]) {
-        languageStats[node.name] += size;
-      } else {
-        languageStats[node.name] = size;
-      }
-    });
-  });
-
-  return languageStats;
+  const languageStats = extractLanguages(repos);
+  const languagePercent = calculatePercent(languageStats);
+  const sortedLanguagesArray = Object.entries(languagePercent).sort(
+    ([, a], [, b]) => b - a,
+  );
+  return Object.fromEntries(sortedLanguagesArray);
 };
