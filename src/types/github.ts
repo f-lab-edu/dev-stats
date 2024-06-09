f7a01e79032a30c6021c156375c08d1b22bf22af
jang_yoonsu <shagrat8546@gmail.com>
2024-06-09 13:00:07 +0900

    feat : Add Profile type to github.ts

--- src/types/github.ts
+++ src/types/github.ts
@@ -2,3 +2,22 @@ export type SearchedUser = {
   login: string;
   avatar_url: string;
 };
+
+export type Profile = {
+  login: string;
+  avatar_url: string;
+  html_url: string;
+  name: string;
+  company: string;
+  blog: string;
+  location: string;
+  email: string;
+  bio: string;
+  twitter_username: string;
+  public_repos: number;
+  public_gists: number;
+  followers: number;
+  following: number;
+  created_at: string;
+  updated_at: string;
+};
