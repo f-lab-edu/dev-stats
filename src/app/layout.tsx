767baf57c76928e7efb359364345de2d848e85ab
jang_yoonsu <shagrat8546@gmail.com>
2024-06-09 12:39:49 +0900

    refactor : Refactor layout component to update body styling in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,12 +32,8 @@ export default function RootLayout({
   children: ReactNode;
 }>) {
   return (
-    <html lang="en" className="bg-blue-100">
-      <body className={inter.className}>
-        <main className="flex flex-col items-center max-w-[1020px] mx-auto h-[100dvh]">
-          {children}
-        </main>
-      </body>
+    <html lang="en" className="bg-blue-100/70">
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
