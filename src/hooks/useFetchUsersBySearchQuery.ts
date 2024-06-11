7b461c4ae309a81bfc343ceefaec97050b53ec5b
jang_yoonsu <shagrat8546@gmail.com>
2024-06-11 22:43:21 +0900

    refactor : Refactor useFetchUsersBySearchQuery hook to improve search functionality

--- src/hooks/useFetchUsersBySearchQuery.ts
+++ src/hooks/useFetchUsersBySearchQuery.ts
@@ -2,38 +2,22 @@ import { useCallback, useEffect, useState, useTransition } from "react";
 
 import { getUsersBySearchQuery } from "@/apis";
 import { debounce } from "@/utils";
-import { AsyncState, SearchedUser } from "@/types";
+import { SearchedUser } from "@/types";
 
 export const useSearchUserQuery = (searchQuery: string) => {
   const [isPending, startTransition] = useTransition();
-  const [searchResult, setSearchResult] = useState<SearchedUser[]>([]);
-  const [isNoResult, setIsNoResult] = useState(false);
+  const [searchResult, setSearchResult] = useState<SearchedUser[] | null>(null);
   const [error, setError] = useState<unknown>(null);
-
-  const isInitialPending =
-    searchQuery.length > 2 && !isNoResult && searchResult.length === 0;
-  const resultState: AsyncState =
-    (!!error && "ERROR") ||
-    (isInitialPending && "LOADING") ||
-    (isNoResult && "NO_RESULT") ||
-    "SUCCESS";
 
   const searchUser = async (searchQuery: string) => {
     if (searchQuery.length < 3) {
-      setSearchResult([]);
+      setSearchResult(null);
       return;
     }
 
     try {
       const response = await getUsersBySearchQuery(searchQuery);
 
-      if (response.total_count === 0) {
-        setIsNoResult(true);
-        setSearchResult([]);
-        return;
-      }
-
-      setIsNoResult(false);
       startTransition(() => {
         setSearchResult(response.items.slice(0, 5));
       });
@@ -52,6 +36,7 @@ export const useSearchUserQuery = (searchQuery: string) => {
   return {
     isPending,
     searchedUsers: searchResult,
-    resultState,
+    isLoading: searchResult === null,
+    isError: !!error,
   };
 };
