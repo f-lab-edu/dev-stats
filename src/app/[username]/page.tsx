a1f7b853cf747cf88587cb50ef9f6b632e8a0282
jang_yoonsu <shagrat8546@gmail.com>
2024-06-13 00:49:38 +0900

    feat: Include contributions in UserPage component

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -10,7 +10,7 @@ type UserPageProps = {
 
 const UserPage = async ({ params }: UserPageProps) => {
   const username = params.username;
-  const { profile, languages, organizations } =
+  const { profile, languages, organizations, contributions } =
     await getDashboardData(username);
 
   return (
@@ -18,6 +18,7 @@ const UserPage = async ({ params }: UserPageProps) => {
       profileData={profile}
       languagesData={languages}
       organizations={organizations}
+      contributions={contributions}
     />
   );
 };
