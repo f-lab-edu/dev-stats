0bf5350be0c384635586d5c6e1db7138f7cf00f0
jang_yoonsu <shagrat8546@gmail.com>
2024-06-19 22:12:36 +0900

    refactor : Refactor DashboardErrorFallback component to use destructuring in props

--- src/components/async/DashboardErrorFallback.tsx
+++ src/components/async/DashboardErrorFallback.tsx
@@ -1,15 +1,8 @@
 "use client";
 
-import { HTMLAttributes } from "react";
-
 import { ErrorFallbackProps } from "./ErrorBoundary";
 
-type DashboardErrorFallbackProps = HTMLAttributes<HTMLElement> &
-  ErrorFallbackProps;
-
-export const DashboardErrorFallback = ({
-  error,
-}: DashboardErrorFallbackProps) => {
+export const DashboardErrorFallback = ({ error }: ErrorFallbackProps) => {
   return (
     <div className="full-size flex-row-center">
       <p>{error.message}</p>
