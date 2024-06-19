88555b14747c3aa49ac9d35b1d60fc73cf816296
jang_yoonsu <shagrat8546@gmail.com>
2024-06-19 22:14:04 +0900

    refactor : Refactor Contribution component to include username prop and remove unused imports

--- src/components/dashboard/Contribution.tsx
+++ src/components/dashboard/Contribution.tsx
@@ -1,13 +1,7 @@
-"use client";
-
-import { HTMLAttributes } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
 import { ContributedRepoType } from "@/types";
-
-import { Section } from "../common";
-import { usePathname } from "next/navigation";
 import { formatCount } from "@/utils";
 
 const GITHUB_URL = "https://www.github.com/";
@@ -16,48 +10,49 @@ const SUFFIX = ")";
 
 type ContributionProps = {
   contributedRepoData: ContributedRepoType[] | null;
-} & HTMLAttributes<HTMLElement>;
+  username: string;
+};
 
 export const Contribution = ({
   contributedRepoData,
-  ...props
+  username,
 }: ContributionProps) => {
-  const username = usePathname().split("/")[1];
+  if (!contributedRepoData) {
+    throw new Error("Failed to get Contribution data.");
+  }
 
   return (
-    <Section title="Contribution" {...props}>
-      <div className="flex-1 grid grid-cols-3 grid-rows-3 mt-1 gap-2">
-        {contributedRepoData?.slice(0, 9).map((repo, index) => (
-          <Link
-            href={`${GITHUB_URL + repo.repository}/pulls?q=author%3A${username}+is%3Apr+is%3Aclosed`}
-            target="_blank"
-            rel="noopener noreferrer"
-            key={index}
-            className="
+    <div className="flex-1 grid grid-cols-3 grid-rows-3 mt-1 gap-2">
+      {contributedRepoData?.slice(0, 9).map((repo, index) => (
+        <Link
+          href={`${GITHUB_URL + repo.repository}/pulls?q=author%3A${username}+is%3Apr+is%3Aclosed`}
+          target="_blank"
+          rel="noopener noreferrer"
+          key={index}
+          className="
               flex justify-center gap-2 px-2 pt-3 pb-1
               min-w-[100px] rounded-lg hover:bg-blue-100/90 cursor-pointer
             "
-          >
-            <div className="flex flex-col gap-3 items-center justify-center">
-              <Image
-                src={repo.avatarUrl}
-                alt={`${repo.repository} image`}
-                width={50}
-                height={50}
-                className="rounded-full max-w-9 max-y-9 flex-shrink bg-white"
-              />
-              <div className="flex flex-col gap-[2px] text-center">
-                <h2 className="text-md font-medium leading-4">
-                  {repo.repository.split("/")[1]}{" "}
-                </h2>
-                <span className="text-[12px] h-[18px] text-gray-400">
-                  {formatCount(repo.stargazerCount, PREFIX, SUFFIX)}
-                </span>
-              </div>
+        >
+          <div className="flex flex-col gap-3 items-center justify-center">
+            <Image
+              src={repo.avatarUrl}
+              alt={`${repo.repository} image`}
+              width={50}
+              height={50}
+              className="rounded-full max-w-9 max-y-9 flex-shrink bg-white"
+            />
+            <div className="flex flex-col gap-[2px] text-center">
+              <h2 className="text-md font-medium leading-4">
+                {repo.repository.split("/")[1]}{" "}
+              </h2>
+              <span className="text-[12px] h-[18px] text-gray-400">
+                {formatCount(repo.stargazerCount, PREFIX, SUFFIX)}
+              </span>
             </div>
-          </Link>
-        ))}
-      </div>
-    </Section>
+          </div>
+        </Link>
+      ))}
+    </div>
   );
 };
