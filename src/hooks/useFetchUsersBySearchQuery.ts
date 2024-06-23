eed6a95d82079047650236d7938a8f1e65c485fd
jang_yoonsu <shagrat8546@gmail.com>
2024-06-23 13:44:13 +0900

    refactor : Refactor useFetchUsersBySearchQuery hook to useQuery and fetchUsers functions

--- src/hooks/useFetchUsersBySearchQuery.ts
+++ src/hooks/useFetchUsersBySearchQuery.ts
@@ -1,57 +1,23 @@
-import { useEffect, useRef, useState, useTransition } from "react";
-
-import { getUsersBySearchQuery } from "@/apis";
+import { useQuery } from "./useQuery";
 import { SearchedUser } from "@/types";
+import { getUsersBySearchQuery } from "@/apis";
+
+export const fetchUsers = async (query: string): Promise<SearchedUser[]> => {
+  const response = await getUsersBySearchQuery(query);
+  return response.items.slice(0, 5);
+};
 
 export const useSearchUserQuery = (searchQuery: string) => {
-  const [isPending, startTransition] = useTransition();
-  const [searchResult, setSearchResult] = useState<SearchedUser[] | null>(null);
-  const [error, setError] = useState<unknown>(null);
-  const previousQuery = useRef<string | null>(null);
-  const abortControllerRef = useRef<AbortController | null>(null);
-
-  useEffect(() => {
-    const searchUser = async (query: string, signal: AbortSignal) => {
-      try {
-        const response = await getUsersBySearchQuery(query, { signal });
-        startTransition(() => {
-          setSearchResult(response.items.slice(0, 5));
-        });
-      } catch (err: unknown) {
-        if (err instanceof Error && err.name !== "AbortError") {
-          setError(err);
-        }
-      }
-    };
-
-    if (searchQuery.length < 3) {
-      setSearchResult(null);
-      return;
-    }
-
-    if (previousQuery.current !== searchQuery) {
-      previousQuery.current = searchQuery;
-      if (abortControllerRef.current) {
-        abortControllerRef.current.abort();
-      }
-
-      const controller = new AbortController();
-      abortControllerRef.current = controller;
-
-      searchUser(searchQuery, controller.signal);
-    }
-
-    return () => {
-      if (abortControllerRef.current) {
-        abortControllerRef.current.abort();
-      }
-    };
-  }, [searchQuery]);
+  const { isPending, isLoading, data, error } = useQuery<SearchedUser[]>({
+    queryKey: ["searchedUsers", searchQuery],
+    queryFn: () => fetchUsers(searchQuery),
+    options: { enabled: searchQuery.length >= 3 },
+  });
 
   return {
     isPending,
-    searchedUsers: searchResult,
-    isLoading: searchResult === null && !error,
+    searchedUsers: data,
+    isLoading,
     isError: !!error,
   };
 };
